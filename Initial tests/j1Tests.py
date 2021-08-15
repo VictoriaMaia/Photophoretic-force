f264b32073e6d83deaa89d663f87fa26e4a74a67
VictoriaMaia <victoria.maia@alu.ufc.br>
2021-08-15 18:44:21 -0300

    Changes J1 function to receive generic parameters

--- Initial tests/j1Tests.py
+++ Initial tests/j1Tests.py
@@ -4,7 +4,9 @@ from asymmetryFactorJ1 import j1 as J
 import numpy as np
 import matplotlib.pyplot as plt
 import plotFunctions
+import math
 
+micro = 10**(-6) 
 
 def testJ1Value():
     m = 1.57 - 0.038j
@@ -12,7 +14,7 @@ def testJ1Value():
     ur = 1
     eps_r2l = J.epsilon(m, ur)
 
-    print(J.J1(x, m, eps_r2l, ur))
+    print(J.J1(J.J1_attributes(x, m, eps_r2l, ur)))
 
 
 def testJ1For3Particles():
@@ -31,9 +33,9 @@ def testJ1For3Particles():
     resultsRed = []
     resultsBlack = []
     for i in x:
-        resultsBlue.append(J.J1(i, mBlue, eps_r2lBlue, ur))
-        resultsRed.append(J.J1(i, mRed, eps_r2lRed, ur))
-        resultsBlack.append(J.J1(i, mBlack, eps_r2lBlack, ur))
+        resultsBlue.append(J.J1(J.J1_attributes(i, mBlue, eps_r2lBlue, ur)))
+        resultsRed.append(J.J1(J.J1_attributes(i, mRed, eps_r2lRed, ur)))
+        resultsBlack.append(J.J1(J.J1_attributes(i, mBlack, eps_r2lBlack, ur)))
 
     plt.figure(figsize=[7,5])
     plt.plot(x, resultsBlue, 'b')
@@ -59,13 +61,12 @@ def testJ1Fig1MACKOWSKI():
     results = []
     rzeros = []
     for i in x:
-        results.append(J.J1(i, m, eps_r2l, ur))
+        results.append(J.J1(J.J1_attributes(i, m, eps_r2l, ur)))
         rzeros.append(0)
 
     plotFunctions.PlotOneGraphic ("", "Eqn (62)", 'g', results, x, -0.10, 0.15, xLabel='Size Parameter x', yLabel='Asymmetry Factor J1')
 
 def testJ1Fig2MACKOWSKI():
-    # micro = 10**(-6) 
     m = 1.57 - 0.38j
     x = np.linspace(0.01, 20, 200)
     ur = 1
@@ -73,7 +74,7 @@ def testJ1Fig2MACKOWSKI():
     results = []
 
     for i in x:
-        results.append(J.J1(i, m, eps_r2l, ur))
+        results.append(J.J1(J.J1_attributes(i, m, eps_r2l, ur)))
 
     plt.figure(figsize=[7,5])
     plt.plot(x, results, 'g', label= "M = 1.57 - 0.38j")
@@ -89,8 +90,134 @@ def testJ1Fig2MACKOWSKI():
     plt.show()
 
 
+def testJ1GaussianBeam():
+    l = 10.63 * micro 
+    k = (2*math.pi) / l
+    z0 = 0
+    s = 0.1
+
+    mBlue = 1.57 - 0.038j
+    mRed = 1.57 - 0.19j
+    mBlack = 1.57 - 0.95j
+
+    x = np.linspace(0.01, 20, 200)
+    ur = 1
+    eps_r2lBlue = J.epsilon(mBlue, ur)
+    eps_r2lRed = J.epsilon(mRed, ur)
+    eps_r2lBlack = J.epsilon(mBlack, ur)
+
+    resultsBlue = []
+    resultsBlueTick = []
+    resultsRed = []
+    resultsRedTick = []
+    resultsBlack = []
+    resultsBlackTick = []
+    for i in x:
+        resultsBlueTick.append(J.J1(J.J1_attributes(i, mBlue, eps_r2lBlue, ur)))
+        resultsBlue.append(J.J1(J.J1_attributes(i, mBlue, eps_r2lBlue, ur, k, z0, s)))
+        resultsRedTick.append(J.J1(J.J1_attributes(i, mRed, eps_r2lRed, ur)))
+        resultsRed.append(J.J1(J.J1_attributes(i,  mRed, eps_r2lRed, ur, k, z0, s)))
+        resultsBlackTick.append(J.J1(J.J1_attributes(i, mBlack, eps_r2lBlack, ur)))
+        resultsBlack.append(J.J1(J.J1_attributes(i, mBlack, eps_r2lBlack, ur, k, z0, s)))
+
+    plt.figure(figsize=[10,5])
+    plt.plot(x, resultsBlueTick, 'b-.', label= "onda plana (g_n = 1)")
+    plt.plot(x, resultsBlue, 'b', label= "gaussian beam")
+    plt.plot(x, resultsRedTick, 'r-.', label= "onda plana")
+    plt.plot(x, resultsRed, 'r', label= "gaussian beam")
+    plt.plot(x, resultsBlackTick, 'g-.', label= "onda plana")
+    plt.plot(x, resultsBlack, 'g', label= "gaussian beam")
+    plt.xlabel('Size Parameter x')
+    plt.ylabel('Asymmetry Factor J1')
+    plt.grid()
+    plt.legend(loc='best')
+    plt.text(12.5, .03, 'M = 1.57 - 0.038j')
+    plt.text(10, -.1, 'M = 1.57 - 0.19j')
+    plt.text(7.5, -.33, 'M = 1.57 - 0.95j')
+    plt.show()
+
+def testJ1GaussianBeam_s():
+    l = 10.63 * micro 
+    k = (2*math.pi) / l
+    z0 = 0
+    m = 1.57 - 0.038j
+    x3 = 3
+    x8 = 8
+
+    s = np.linspace(0.01, 0.16, 200)
+    ur = 1
+    eps_r2l = J.epsilon(m, ur)
+
+    resultsX3 = []
+    resultsX3Tick = []
+    resultsX8 = []
+    resultsX8Tick = []
+
+    for i in s:
+        resultsX3Tick.append(J.J1(J.J1_attributes(x3, m, eps_r2l, ur)))
+        resultsX3.append(J.J1(J.J1_attributes(x3, m, eps_r2l, ur, k, z0, i)))
+        resultsX8Tick.append(J.J1(J.J1_attributes(x8, m, eps_r2l, ur)))
+        resultsX8.append(J.J1(J.J1_attributes(x8,  m, eps_r2l, ur, k, z0, i)))
+        
+        
+    plt.figure(figsize=[10,5])
+    plt.plot(s, resultsX3Tick, 'k-.', label= "onda plana (g_n = 1)")
+    plt.plot(s, resultsX3, 'k', label= "gaussian beam")
+    plt.plot(s, resultsX8Tick, 'b-.', label= "onda plana")
+    plt.plot(s, resultsX8, 'b', label= "gaussian beam")
+    plt.xlabel('Confinement factor s')
+    plt.ylabel('Asymmetry Factor J1')
+    plt.xlim(0, 0.165)
+    plt.grid()
+    plt.legend(loc='best')
+    plt.text(0.1, .045, 'x=3')
+    plt.text(0.06, .02, 'x=8')
+    plt.show()
+
+def testJ1GaussianBeam_z0():
+    l = 10.63 * micro 
+    k = (2*math.pi) / l
+
+    m = 1.57 - 0.038j
+    s = 0.16
+
+    x1 = 0.1
+    x3 = 3
+    x8 = 8
+
+    ur = 1
+    eps_r2l = J.epsilon(m, ur)
+
+    z0 = np.linspace(-70*micro, 70*micro, 250)
+    # z0 = np.linspace(0, 60, 200)
+
+    resultsX1 = []
+    resultsX3 = []
+    resultsX8 = []
+
+    for i in z0:
+        resultsX1.append(J.J1(J.J1_attributes(x1, m, eps_r2l, ur, k, i, s))*500)
+        resultsX3.append(J.J1(J.J1_attributes(x3, m, eps_r2l, ur, k, i, s)))
+        resultsX8.append(J.J1(J.J1_attributes(x8, m, eps_r2l, ur, k, i, s)))
+        
+
+    z0 = np.linspace(-70, 70, 250)
+    plt.figure(figsize=[10,5])
+    plt.plot(z0, resultsX1, 'b', label= "x = 0.1 * 500 ")
+    plt.plot(z0, resultsX3, 'r', label= "x = 3")
+    plt.plot(z0, resultsX8, 'g', label= "x = 8")
+    plt.xlabel('Relative position z_0 (u_m) ')
+    plt.ylabel('Asymmetry Factor J1')
+    plt.grid()
+    plt.legend(loc='best')
+    plt.show()
+
+
 if __name__ == '__main__':
     testJ1For3Particles()
     testJ1Value()
     testJ1Fig1MACKOWSKI()
     testJ1Fig2MACKOWSKI()
+    testJ1GaussianBeam()
+    testJ1GaussianBeam_s()
+    testJ1GaussianBeam_z0()
--- asymmetryFactorJ1/j1.py
+++ asymmetryFactorJ1/j1.py
@@ -4,6 +4,27 @@ from . import rn as RN
 from . import sn as SN
 from . import nMax as NMAX
 import numpy as np
+from mpmath import exp
+
+class J1_attributes:
+    def __init__(self, x, M, epislon2l, ur, k="False", z0="False", s="False") -> None:
+        self.x = x
+        self.M = M
+        self.epislon2l = epislon2l
+        self.ur = ur
+        self.k = k
+        self.z0 = z0
+        self.s = s
+        pass
+
+    def printJ1Attributes(self):
+        print("x: ", self.x)
+        print("M: ", self.M)
+        print("Eps: ", self.epislon2l)
+        print("Ur: ", self.ur)
+        print("k: ", self.k)
+        print("z0: ", self.z0)
+        print("s: ", self.s)
 
 def epsilon(m, ur):
     epsilon = (m**2) / ur
@@ -14,32 +35,101 @@ def eta_r(m, ur):
     divUrEpsilon = ur/epsilon 
     return divUrEpsilon ** (1/2)    
 
-def J1(x, M, epislon2l, ur):
-    firstTerm = (3 * epislon2l) / ((abs(M) ** 2) * (x ** 3))
-    summationResult = 0
-    nMax = NMAX.ceilingX(x)
-    etaR = eta_r(M, ur)
-    mod2EtaR = abs(etaR) ** 2
+def varQ(z0, s, k):
+    i = 1j
+    secondTermSub = i*2*z0*(s**2)*k
     
+    return 1 / (1 + secondTermSub)
+
+def varG_nGaussianBeam (n, k, x, z0, s):
+    i = 1j
+    
+    q = varQ(z0, s, k)
+    
+    arg1 = s**2
+    arg2 = ((n+(1/2))**2)
+    argExp = -q*arg1*arg2
+    resultExp = exp(argExp)
+
+    argE = i*k*z0
+    resultE = exp(argE)
+    
+    return q*resultExp*resultE
+
+def Summation(parameters):
+    summationResult = 0
+    nMax = NMAX.ceilingX(parameters.x)
+    etaR = eta_r(parameters.M, parameters.ur)
+    mod2EtaR = abs(etaR) ** 2
+
     for i in range(1, (nMax+1)):
-        cn = CN.varC_n (ur, M, x, i) 
+        cn = CN.varC_n (parameters.ur, parameters.M, parameters.x, i) 
         conjCn = np.conj(cn)
-        cn1 = CN.varC_n (ur, M, x, (i+1))
+        cn1 = CN.varC_n (parameters.ur, parameters.M, parameters.x, (i+1))
         conjCn1 = np.conj(cn1)
-        rn = RN.Rn (M, x, i)
-        rn1 = RN.Rn (M, x, (i+1))
-        dn = DN.varD_n (ur, M, x, i)
+        rn = RN.Rn (parameters.M, parameters.x, i)
+        rn1 = RN.Rn (parameters.M, parameters.x, (i+1))
+        dn = DN.varD_n (parameters.ur, parameters.M, parameters.x, i)
         conjDn = np.conj(dn)
-        dn1 = DN.varD_n (ur, M, x, (i+1))
+        dn1 = DN.varD_n (parameters.ur, parameters.M, parameters.x, (i+1))
         conjDn1 = np.conj(dn1)
-        sn = SN.Sn (M, x, i)
+        sn = SN.Sn (parameters.M, parameters.x, i)
         
-        fristTermFor = ((i*(i+2)) / M) * ((cn1*conjCn*rn1) + (mod2EtaR*dn1*conjDn*rn))
+        fristTermFor = ((i*(i+2)) / parameters.M) * ((cn1*conjCn*rn1) + (mod2EtaR*dn1*conjDn*rn))
         secondTermFor = ((i*(i+2)) / (i+1)) * ((cn*conjCn1) + (mod2EtaR*dn1*conjDn))
         thirdTermFor =  np.conj(etaR) * (((2*i)+1) / (i*(i+1))) * (cn*conjDn)
         
         result = fristTermFor - ((secondTermFor + thirdTermFor) * sn)
         
         summationResult += result
+    
+    return summationResult
+
+
+def SummationWithGn(parameters):
+    nMax = NMAX.ceilingX(parameters.x)
+    etaR = eta_r(parameters.M, parameters.ur)
+    mod2EtaR = abs(etaR) ** 2
+    summationResult = 0
+    
+    for i in range(1, (nMax+1)):
+        cn = CN.varC_n (parameters.ur, parameters.M, parameters.x, i) 
+        conjCn = np.conj(cn)
+        cn1 = CN.varC_n (parameters.ur, parameters.M, parameters.x, (i+1))
+        conjCn1 = np.conj(cn1)
+        gn = varG_nGaussianBeam(i, parameters.k, parameters.x, parameters.z0, parameters.s)
+        conjGn = np.conj(gn)
+        gn1 = varG_nGaussianBeam((i+1), parameters.k, parameters.x, parameters.z0, parameters.s)
+        conjGn1 = np.conj(gn1)
+        rn = RN.Rn (parameters.M, parameters.x, i)
+        rn1 = RN.Rn (parameters.M, parameters.x, (i+1))
+        dn = DN.varD_n (parameters.ur, parameters.M, parameters.x, i)
+        conjDn = np.conj(dn)
+        dn1 = DN.varD_n (parameters.ur, parameters.M, parameters.x, (i+1))
+        conjDn1 = np.conj(dn1)
+        sn = SN.Sn (parameters.M, parameters.x, i)
+        
+        fristTermFor = ((i*(i+2)) / parameters.M) * ((gn1*conjGn*cn1*conjCn*rn1) + (mod2EtaR*gn1*conjGn*dn1*conjDn*rn))
+        secondTermFor = ((i*(i+2)) / (i+1)) * ((gn*conjGn1*cn*conjCn1) + (mod2EtaR*gn1*conjGn*dn1*conjDn))
+        thirdTermFor = np.conj(etaR) * (((2*i)+1) / (i*(i+1))) * (abs(gn)**2) * (cn*conjDn)
+        
+        result = fristTermFor - ((secondTermFor + thirdTermFor) * sn)
+        
+        summationResult += result
+    
+    return summationResult
+
+
+def J1(parameters):
+    if type(parameters) != J1_attributes:
+        print("Error: parameters is not of type class J1_attributes")
+        return 0
+    
+    firstTerm = (3 * parameters.epislon2l) / ((abs(parameters.M) ** 2) * (parameters.x ** 3))
+
+    if parameters.k == "False" and parameters.z0 == "False" and parameters.s == "False":
+        summationResult = Summation(parameters)
+    else:
+        summationResult = SummationWithGn(parameters) 
         
     return firstTerm * summationResult.imag
