ff998c3de72a4205e0c0a0baef37a571c958c414
victoria_maia <victoria.maia.ec@gmail.com>
2023-03-24 22:50:41 -0300

    Add j1 time measurement

--- AsymmetryFactor/AsymmetryFactor/j1.py
+++ AsymmetryFactor/AsymmetryFactor/j1.py
@@ -3,6 +3,9 @@ from .particle.particle_class import ParticleAttributes
 from .beams.beam_class import BeamAttributes
 from .beams import *
 import numpy as np
+from timeit import default_timer as timer
+
+dic_time = {'ponto':0, 'gn': 0, 'gn1': 0, 'ot': 0, 'op':0, 'j1':0}
 
 
 def summation(particle, beam):
@@ -65,6 +68,88 @@ def summation(particle, beam):
     return summation_result
 
 
+def summation_with_compute_time(particle, beam):
+    time_gn = 0
+    time_gn1 = 0
+    time_other_terms = 0
+    time_operations = 0
+
+
+    start_j1 = timer() 
+    
+    n_max = ceiling_x(particle.x)
+    v_eta_r = eta_r(particle.m, particle.ur)
+    mod2_eta_r = abs(v_eta_r) ** 2
+    summation_result = 0
+
+    for i in range(1, (n_max+1)):
+        # variables computed in i position
+        start_ot = timer()
+        vars_i = compute_variables(particle, i)
+        time_other_terms += timer() - start_ot
+
+        if isinstance(beam, GaussAttributes):
+            gn = beam.gn_beam(i)
+            conj_gn = np.conj(gn)
+            gn1 = beam.gn_beam((i+1))
+            conj_gn1 = np.conj(gn1)
+        elif isinstance(beam, BesselAttributes):
+            gn = gn_bessel_beam(i, beam.k, beam.z0, beam.angle)
+            conj_gn = np.conj(gn)
+            gn1 = gn_bessel_beam((i+1), beam.k, beam.z0, beam.angle)
+            conj_gn1 = np.conj(gn1)
+        elif isinstance(beam, FrozenWaveAttributes):
+            
+            start_gn = timer()
+            gn = gn_frozen_wave_beam(i, beam.n, beam.k, beam.z0, beam.l, beam.q)
+            time_gn += timer() - start_gn
+
+            conj_gn = np.conj(gn)
+
+            start_gn1 = timer()
+            gn1 = gn_frozen_wave_beam((i+1), beam.n, beam.k, beam.z0, beam.l, beam.q)
+            time_gn1 += timer() - start_gn1
+
+            conj_gn1 = np.conj(gn1)
+        else:
+            gn = 1
+            conj_gn = 1
+            gn1 = 1
+            conj_gn1 = 1
+
+
+        start_operations = timer()
+
+        first_term_for = ((i*(i+2)) / particle.m) * \
+                         (
+                                 (gn1 * conj_gn * vars_i.cn1 * vars_i.conj_cn * vars_i.rn1) +
+                                 (mod2_eta_r * gn1 * conj_gn * vars_i.dn1 * vars_i.conj_dn * vars_i.rn)
+                         )
+
+        second_term_for = ((i*(i+2)) / (i+1)) * \
+                          (
+                                  (gn * conj_gn1 * vars_i.cn * vars_i.conj_cn1) +
+                                  (mod2_eta_r * gn1 * conj_gn * vars_i.dn1 * vars_i.conj_dn)
+                          )
+
+        third_term_for = np.conj(v_eta_r) * (((2*i)+1) / (i*(i+1))) * (abs(gn)**2) * (vars_i.cn * vars_i.conj_dn)
+        
+        result = first_term_for - ((second_term_for + third_term_for) * vars_i.sn)
+        
+        summation_result += result
+
+        time_operations += timer() - start_operations
+    
+    
+    dic_time['j1'] = timer() - start_j1
+    dic_time['gn'] = time_gn
+    dic_time['gn1'] = time_gn1
+    dic_time['ot'] = time_other_terms
+    dic_time['op'] = time_operations
+
+    return summation_result, dic_time
+
+
 def j1(particle, beam):    
     """
     TODO: add description
@@ -79,10 +164,14 @@ def j1(particle, beam):
 
         first_term = (3 * epsilon2l) / ((abs(particle.m) ** 2) * (particle.x ** 3))
 
-        summation_result = summation(particle, beam)
+        # summation_result = summation(particle, beam)
+        summation_result, dic_time = summation_with_compute_time(particle, beam)
+        
     else:
         print("Error. Type of particle invalid. Type is", type(particle))
         print("And is expected: ", ParticleAttributes)
         return 0
-        
-    return first_term * summation_result.imag
+    
+    result = first_term * summation_result.imag
+    
+    return result, dic_time
--- AsymmetryFactor/simulations/frozen_wave_beam.py
+++ AsymmetryFactor/simulations/frozen_wave_beam.py
@@ -8,15 +8,22 @@ from simulations.create_graph import plot_graphic
 from simulations.time_operations import *
 from timeit import default_timer as timer
 from tqdm import tqdm
+import csv
 import numpy as np
 import math
 
+mili = 10**(-3)
+micro = 10**(-6) 
+nano = 10**(-9)
 orange = '#ff6800'
 
-def j1_frozen_wave_beam_with_three_size_parameters():
-    micro = 10**(-6) 
-    nano = 10**(-9)
+calculate_average = False
+max_executions = 10
+qnt_points = 50
 
+dic_time = {'ponto':0, 'gn': 0, 'gn1': 0, 'ot': 0, 'op':0, 'j1':0}
+
+def j1_frozen_wave_beam_with_three_size_parameters():
     var_lambda = 1064 * nano
         
     ur = 1
@@ -39,42 +46,151 @@ def j1_frozen_wave_beam_with_three_size_parameters():
     results_x3 = []
     results_x8 = []
 
-    z0 = np.linspace(200*micro, 400*micro, 50)
+    z0 = np.linspace(0, 400*micro, qnt_points)
 
-    pbar = tqdm(colour=orange, total=len(z0), desc="Test 1. Calculating")
-    start = timer()
-    for i in z0:
-        # print(i)
-        # print(f'x=0.1 : {j1(particle_x_01, FrozenWaveAttributes(k, i, q, l, n))}')
-        # print(f'x=3 : {j1(particle_x_3, FrozenWaveAttributes(k, i, q, l, n))}')
-        # print(f'x=8 : {j1(particle_x_8, FrozenWaveAttributes(k, i, q, l, n))}')
-        # return
-        results_x01.append(j1(particle_x_01, FrozenWaveAttributes(k, i, q, l, n))*2500)
-        results_x3.append(j1(particle_x_3, FrozenWaveAttributes(k, i, q, l, n)))
-        results_x8.append(j1(particle_x_8, FrozenWaveAttributes(k, i, q, l, n)))
+    if calculate_average:
+        total_time = 0
+        
+        pbar = tqdm(colour=orange, total=max_executions, desc="Calculating points", leave=False)
+        for i in range(max_executions):            
+            start = timer()
+            
+            for i in z0:
+                results_x01.append(j1(particle_x_01, FrozenWaveAttributes(k, i, q, l, n))*2500)
+                results_x3.append(j1(particle_x_3, FrozenWaveAttributes(k, i, q, l, n)))
+                results_x8.append(j1(particle_x_8, FrozenWaveAttributes(k, i, q, l, n)))
+            
+            total_time += timer()-start
+            pbar.update()
+    
+        pbar.refresh()
+        pbar.close()  
+        average = total_time/max_executions      
+        print(average)  
+    
+        print("Total time: ", end="")
+        convert_time_to_more_readable(average)
+
+    else:
+        pbar = tqdm(colour=orange, total=len(z0), desc="Calculating")
+        for i in z0:
+            results_x01.append(j1(particle_x_01, FrozenWaveAttributes(k, i, q, l, n))*2500)
+            results_x3.append(j1(particle_x_3, FrozenWaveAttributes(k, i, q, l, n)))
+            results_x8.append(j1(particle_x_8, FrozenWaveAttributes(k, i, q, l, n)))
+            pbar.update()
+
+        pbar.refresh()
+        pbar.close()
+
+        z0 = np.linspace(0, 400, qnt_points)
+
+        results = [results_x01, results_x3, results_x8]
+        x_label = "Size Parameter x"
+        y_label = "Asymmetry Factor $J_1(x)$"
+        legend = ["x = 0.1", "x = 3", "x = 8"]
+        plot_graphic(results, z0, x_label, y_label, legend)
+    
+
+def j1_frozen_wave_with_varing_z0_values():
+    var_lambda = 1064 * nano
+        
+    k = (2*math.pi) / var_lambda
+    n = -75
+    q = 0.8*k
+    l = 400*micro    
+
+    fw_l2 = FrozenWaveAttributes(k, l/2, q, l, n)
+    fw_l4 = FrozenWaveAttributes(k, l/4, q, l, n)
+
+    ur = 1
+    m_038 = 1.57 - 0.038j
+    x = np.linspace(0.1, 20, qnt_points)
+
+    results_l2 = []
+    results_l4 = []
+
+    pbar = tqdm(colour=orange, total=len(x), desc="Calculating")
+    for i in x:
+        results_l2.append(j1(ParticleAttributes(i, m_038, ur), fw_l2))
+        results_l4.append(j1(ParticleAttributes(i, m_038, ur), fw_l4)*250)
         pbar.update()
 
-    time = timer()-start
     pbar.refresh()
     pbar.close()
 
-    print("time: ", convert_time_to_more_readable(time))
-    print(z0)
-    print("**************************")
-    print(results_x01)
-    print("**************************")
-    print(results_x3)
-    print("**************************")
-    print(results_x8)
-
-    results = [results_x01, results_x3, results_x8]
+        
+    results = [results_l2, results_l4]
     x_label = "Size Parameter x"
     y_label = "Asymmetry Factor $J_1(x)$"
-    legend = ["x = 0.1", "x = 3", "x = 8"]
-    z0 = np.linspace(0, 400, 50)
-    plot_graphic(results, z0, x_label, y_label, legend)
+    legend = ["$z_0 = L/2$", r'$z_0 = L/4 \times 250$']
+    plot_graphic(results, x, x_label, y_label, legend)
+
+
+
+def j1_frozen_wave_time_calculate():
+    global dic_time
+    var_lambda = 1064 * nano
+        
+    k = (2*math.pi) / var_lambda
+    n = -75
+    q = 0.8*k
+    l = 400*micro    
+
+    fw_l2 = FrozenWaveAttributes(k, l/2, q, l, n)
+    fw_l4 = FrozenWaveAttributes(k, l/4, q, l, n)
+
+    ur = 1
+    m_038 = 1.57 - 0.038j
+    x = np.linspace(0.1, 20, qnt_points)
+
+    results_l2 = []
+
+    time_file_name = "./simulations/outputs/time_result/"+ "j1_frozen_wave_with_varing_z0_values" + ".csv"
+    time_file = open(time_file_name, 'w', newline='')
+    writer_csv_file = csv.DictWriter(time_file, fieldnames= ["ponto", "j1", "gn", "gn1", "ot", "op"])
+    writer_csv_file. writeheader()
+
+    pbar = tqdm(colour=orange, total=len(x), desc="Calculating")
+    for i in x:
+        j1_value, dic_receive = j1(ParticleAttributes(i, m_038, ur), fw_l2)
+        
+        dic_receive["ponto"] = i
+        writer_csv_file.writerow(dic_receive)
+
+        results_l2.append(j1_value)
+
+        dic_time["j1"] += dic_receive["j1"]
+        dic_time["gn"] += dic_receive["gn"]
+        dic_time["gn1"] += dic_receive["gn1"]
+        dic_time["ot"] += dic_receive["ot"]
+        dic_time["op"] += dic_receive["op"]
+
+        pbar.update()
+
+    pbar.refresh()
+    pbar.close()
+
+
+    dic_time["j1"] = dic_time["j1"]/qnt_points  
+    dic_time["gn"] = dic_time["gn"]/qnt_points  
+    dic_time["gn1"] = dic_time["gn1"]/qnt_points  
+    dic_time["ot"] = dic_time["ot"]/qnt_points  
+    dic_time["op"] = dic_time["op"]/qnt_points   
+
+    dic_time["ponto"] = "media"
+    writer_csv_file.writerow(dic_time)
+
+    time_file.close()
     
+    results = [results_l2]
+    x_label = "Size Parameter x"
+    y_label = "Asymmetry Factor $J_1(x)$"
+    legend = ["$z_0 = L/2$"]
+    plot_graphic(results, x, x_label, y_label, legend)
+
+
 
 if __name__ == '__main__':
     j1_frozen_wave_beam_with_three_size_parameters()
-    
+    # j1_frozen_wave_with_varing_z0_values()
+    # j1_frozen_wave_time_calculate()
